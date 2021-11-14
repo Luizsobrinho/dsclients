474ef6e8a4eba7cb3bf2a38b78bdf948ae1f51bc
José Luiz dos Santos Sobrinho <luizsobrinho3000@hotmail.com>
2021-11-14 12:12:33 -0300

    Init project, FindByPage,FindByID

---
+++ src/main/java/com/devsuperior/dsclients/resource/ClientResource.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dsclients.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsclients.dto.ClientDTO;
+import com.devsuperior.dsclients.service.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+	@Autowired
+	private ClientService service;
+
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> clients = service.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(clients);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO client = service.findById(id);
+		return ResponseEntity.ok().body(client);
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vinicius Otávio Araújo', '37474053209', 4500.0 ,TIMESTAMP WITH TIME ZONE '2001-08-14T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Diogo Mário Leandro da Mota', '22996012879', 3500.0 ,TIMESTAMP WITH TIME ZONE '1946-12-02T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Renato Alexandre Lucca Assis', '65462933584', 6500.0 ,TIMESTAMP WITH TIME ZONE '1981-05-21T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Márcio Victor Corte Real', '23332351796', 3500.0 ,TIMESTAMP WITH TIME ZONE '1951-01-22T20:50:07.12345Z',21);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Elias Miguel Augusto Campos', '93159281132', 3500.0 ,TIMESTAMP WITH TIME ZONE '1985-11-16T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bernardo Anthony Nunes', '18497812573', 7500.0 ,TIMESTAMP WITH TIME ZONE '1956-11-07T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Adriana Emily Barbosa', '14845516802', 2500.0 ,TIMESTAMP WITH TIME ZONE '1981-04-07T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Manoel Erick Carlos Rodrigues', '35784668595', 12500.0 ,TIMESTAMP WITH TIME ZONE '1969-11-23T20:50:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sebastião Carlos Eduardo Pietro da Luz', '45555764383', 72500.0 ,TIMESTAMP WITH TIME ZONE '1989-03-21T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Cláudia Teresinha Baptista', '50680556893', 8500.0 ,TIMESTAMP WITH TIME ZONE '1942-02-17T20:50:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Geraldo Danilo Carlos Porto', '23312274540', 5500.0 ,TIMESTAMP WITH TIME ZONE '1980-10-13T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eloá Bruna Rita Novaes', '90716349302', 3500.0 ,TIMESTAMP WITH TIME ZONE '1972-02-23T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Luís Caio Fernando Silva', '20368030750', 7500.0 ,TIMESTAMP WITH TIME ZONE '1992-07-20T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tânia Analu Cecília Castro', '06672188908', 3500.0 ,TIMESTAMP WITH TIME ZONE '1959-12-17T20:50:07.12345Z', 0);
+
+
